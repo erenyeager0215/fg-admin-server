bba7dfefc178270efa3a2346557eaca3beb25af5
naoyuki-hayashi <zlin08022@gmail.com>
2024-03-26 21:26:39 +0900

    corsの設定と外部ドメインからのAPI打鍵確認

--- src/Controller/RestaurantsController.php
+++ src/Controller/RestaurantsController.php
@@ -5,13 +5,25 @@ namespace App\Controller;
 use App\Entity\Testdb;
 use App\Repository\TestdbRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\StreamedJsonResponse;
 
 class RestaurantsController extends AbstractController{
+    #[Route('/testdb',name:'product_create',methods:['POST'])]
+    public function create(EntityManagerInterface $entityManager):StreamedJsonResponse{
+        $request = Request::createFromGlobals();
+        $requestBody = $request->getPayload();
+        $testdb = new Testdb();
+        $testdb->setName($requestBody->get('name'));
+        $testdb->setAddress($requestBody->get('address'));
+        
+        $entityManager->persist($testdb);
+        $entityManager->flush();
+        return new StreamedJsonResponse($requestBody);
+    }
 
     #[Route('/testdb/{id}',name:'product_show',methods:['GET'])]
     public function show(TestdbRepository $testdbRepository,int $id):Response{
